00875cab3719dca47367334c0d58e06caf6c5340
Moses Karani <mk@Mosess-MacBook-Pro.local>
2025-01-24 16:29:05 +0300

    implementing home

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'AchlaShop'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Do it .Do it Well. Do it fast.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'final alx project'
