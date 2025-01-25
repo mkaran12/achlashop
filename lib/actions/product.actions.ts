b997979280d3b90c2f074e8e76443fdd2239fa25
Moses Karani <mk@Mosess-MacBook-Pro.local>
2025-01-25 10:09:07 +0300

    adding product

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import Product from '@/lib/db/models/product.model'
+import { connectToDatabase } from '..'
+
+export async function getAllCategories() {
+  await connectToDatabase()
+  const categories = await Product.find({ isPublished: true }).distinct(
+    'category'
+  )
+  return categories
+}
+export async function getProductsForCard({
+  tag,
+  limit = 4,
+}: {
+  tag: string
+  limit?: number
+}) {
+  await connectToDatabase()
+  const products = await Product.find(
+    { tags: { $in: [tag] }, isPublished: true },
+    {
+      name: 1,
+      href: { $concat: ['/product/', '$slug'] },
+      image: { $arrayElemAt: ['$images', 0] },
+    }
+  )
+    .sort({ createdAt: 'desc' })
+    .limit(limit)
+  return JSON.parse(JSON.stringify(products)) as {
+    name: string
+    href: string
+    image: string
+  }[]
+}
